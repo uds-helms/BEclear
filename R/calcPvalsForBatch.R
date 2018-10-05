aa98bc3742ffa329f6ebbbf69d1390311524d85b
David Rasp <david.j.rasp@gmail.com>
2018-10-05 09:49:36 +0200

    changes in calculating the p-values

--- R/calcPvalsForBatch.R
+++ R/calcPvalsForBatch.R
@@ -20,13 +20,24 @@ calcPvalsForBatch <- function(batch, samples, data) {
                                            nomatch=0][, .(feature, beta.value)]
     
     flog.debug("Performing a Kolmogorov-Smirnov test to obtain the p-value")
-    suppressWarnings(
-        p_values <- DT_batch[ ,if(all(is.na(beta.value))){0.0} 
-                              else{ks.test(beta.value, 
-                                           DT_other[feature==feature, 
-                                                    beta.value], 
-                                           exact = FALSE)$p.value}, 
-                              by=.(feature)])
+    
+    p_values <- vector(mode = "numeric", length = length(unique(data$feature)))
+    i <- 1
+    for (f in unique(data$feature)){
+        flog.debug(paste("Calculating for feature", feature))
+        p_values[i] <- ks.test(DT_batch[feature == f, beta.value],
+                               DT_other[feature == f, beta.value])
+        
+        i <- i + 1
+    }
+    
+    # suppressWarnings(
+    #     p_values <- DT_batch[ ,if(all(is.na(beta.value))){0.0} 
+    #                           else{ks.test(beta.value, 
+    #                                        DT_other[feature==feature, 
+    #                                                 beta.value], 
+    #                                        exact = FALSE)$p.value}, 
+    #                           by=.(feature)])
     DF <- data.frame(p_values$V1, row.names = p_values$feature)
     colnames(DF) <- batch
     return(DF)
