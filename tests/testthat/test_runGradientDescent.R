a555fc14a2866d67e7a9fbdfd63f0dd0b67ae3d7
David Rasp <david.j.rasp@gmail.com>
2018-11-16 11:22:36 +0100

    fix of the tests regarding the is and js values #15

--- tests/testthat/test_runGradientDescent.R
+++ tests/testthat/test_runGradientDescent.R
@@ -16,11 +16,11 @@ testthat::test_that("long D matrix, only integer values given",{
     
     res1<-runGradientDescent(L = matrix(c(5,6)), R = t(matrix(c(7,4,8))), 
                   D = matrix(c(7,5,3,1,3,4), nrow = 2),
-                  lambda = 0.2, is = c(1,2,1,2), js = c(1,1,2,2), 
+                  lambda = 0.2, is = c(1,2,1,2,1,2), js = c(1,1,2,2,3,3), 
                   gamma = 0.01, r=1,epochs = 5, block = 1)
     
-    testthat::expect_equal(res1$L, matrix(c(-0.448, -1.109)), tolerance = .01)
-    testthat::expect_equal(res1$R, t(matrix(c(-0.551, -0.530, 7.91))), tolerance = .01)
+    testthat::expect_equal(res1$L, matrix(c(-6.12, -7.76)), tolerance = .01)
+    testthat::expect_equal(res1$R, t(matrix(c(-0.831, -0.266, -0.504))), tolerance = .01)
 }
 )
 
@@ -28,10 +28,10 @@ testthat::test_that("wide D matrix, only integer values given",{
     
     res1<-runGradientDescent(L = matrix(c(5,1,2)), R = t(matrix(c(3,3))), 
                   D = matrix(c(5,6,10,11,3,1), nrow = 3),
-                  lambda = 0.2, is = c(1,2,1,2), js = c(1,1,2,2), 
+                  lambda = 0.2, is = c(1,2,3,1,2,3), js = c(1,1,1,2,2,2), 
                   gamma = 0.01, r=1, epochs = 5, block = 1)
     
-    testthat::expect_equal(res1$L, matrix(c(3.88449 , 1.50, 2.00)), tolerance = .01)
-    testthat::expect_equal(res1$R, t(matrix(c(1.69, 2.65))), tolerance = .01)
+    testthat::expect_equal(res1$L, matrix(c(3.73, 1.68, 2.20)), tolerance = .01)
+    testthat::expect_equal(res1$R, t(matrix(c(2.26, 2.26))), tolerance = .01)
 }
 )
